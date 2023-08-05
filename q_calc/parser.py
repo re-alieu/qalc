4006fb8e40a34cf20cf32279e48966cfbb456793
Ian <shiyan3@msu.edu>
2023-08-04 23:52:36 -0400

    add capability to create parameter tuple.
    add basic function calls

--- q_calc/parser.py
+++ q_calc/parser.py
@@ -1,4 +1,8 @@
 import logging
+import re
+
+import numpy as np
+import quaternion as qn
 
 logger = logging.getLogger(__name__)
 
@@ -6,7 +10,7 @@ logger = logging.getLogger(__name__)
 tokens = (
     'NAME','NUMBER',
     'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
-    'LPAREN','RPAREN',
+    'LPAREN','RPAREN','COMMA'
     )
 
 # Tokens
@@ -16,16 +20,28 @@ t_MINUS   = r'-'
 t_TIMES   = r'\*'
 t_DIVIDE  = r'/'
 t_EQUALS  = r'='
+t_COMMA   = r','
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
 t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
+numbers_pattern = re.compile("([\d\.]+)([ijk]?)")
+bases = [qn.quaternion(0,1,0,0),qn.quaternion(0,0,1,0), qn.quaternion(0,0,0,1)]
+
 def t_NUMBER(t):
-    r'\d+' # we use these docstrings to define the matching pattern for each token type
+    r'([\d\.]+)([ijk]?)' # we use these docstrings to define the matching pattern for each token type
     try:
-        t.value = int(t.value)
+        p = numbers_pattern.split(t.value)
+        a1 = float(p[1])
+        if p[2] == 'i':
+            a1 = a1 * bases[0]
+        elif p[2] == 'j':
+            a1 = a1 * bases[1]
+        elif p[2] == 'k':
+            a1 = a1 * bases[2]
+        t.value = a1
     except ValueError:
-        print("Integer value too large %d", t.value)
+        print("Invalid numerical value", t.value)
         t.value = 0
     return t
 
@@ -48,6 +64,7 @@ lexer = lex.lex()
 # Parsing rules
 
 precedence = (
+    ('left', 'COMMA'),
     ('left','PLUS','MINUS'),
     ('left','TIMES','DIVIDE'),
     ('right','UMINUS'),
@@ -57,26 +74,59 @@ precedence = (
 names = { }
 
 def p_statement_assign(t):
-    'statement : NAME EQUALS expression'
+    'statement : NAME EQUALS expression' # and these to define
     names[t[1]] = t[3]
 
 def p_statement_expr(t):
     'statement : expression'
     print(t[1])
 
+def tuple_reduce(t):
+    if type(t) == tuple:
+        if len(t) > 0:
+            return t[-1]
+        else:
+            return np.nan
+    else:
+        return t
+
+functions = {
+    'cos': np.cos,
+    'sin': np.sin,
+}
+
+def p_experssion_func(t):
+    r'expression : NAME expression'
+    if t[1] in functions:
+        func_to_call = functions[t[1]]
+        if type(t[2]) == tuple:
+            t[0] = func_to_call(*t[2])
+        else:
+            t[0] = func_to_call(t[2])
+    else:
+        print("unknown function:", t[1])
+        return np.nan
+
 def p_expression_binop(t):
     '''expression : expression PLUS expression
                   | expression MINUS expression
                   | expression TIMES expression
+                  | expression COMMA expression
                   | expression DIVIDE expression'''
-    if t[2] == '+'  : t[0] = t[1] + t[3]
-    elif t[2] == '-': t[0] = t[1] - t[3]
-    elif t[2] == '*': t[0] = t[1] * t[3]
-    elif t[2] == '/': t[0] = t[1] / t[3]
+    if t[2] == '+'  : t[0] = tuple_reduce(t[1]) + tuple_reduce(t[3])
+    elif t[2] == '-': t[0] = tuple_reduce(t[1]) - tuple_reduce(t[3])
+    elif t[2] == '*': t[0] = tuple_reduce(t[1]) * tuple_reduce(t[3])
+    elif t[2] == '/': t[0] = tuple_reduce(t[1]) / tuple_reduce(t[3])
+    elif t[2] == t_COMMA:
+        if type(t[1]) == tuple:
+            t[0] = (*t[1], t[3])
+        else:
+            t[0] = (t[1], t[3])
+
 
 def p_expression_uminus(t):
     'expression : MINUS expression %prec UMINUS'
-    t[0] = -t[2]
+    t[0] = -tuple_reduce(t[2])
 
 def p_expression_group(t):
     'expression : LPAREN expression RPAREN'
